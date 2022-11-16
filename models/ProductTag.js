84ffbdc61687653685fef265dc86570b1f0e987f
Jon Beck <jonnydbeck@gmail.com>
2022-11-16 02:41:44 -0500

    Associations and Touchups

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,13 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    product_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+        unique: false
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      references: {
+        model: 'Tag',
+        key: 'id',
+        unique: false
+      }
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Products'
+})
 
 module.exports = {
   Product,
