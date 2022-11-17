0d10308e7c1e483f98d49701fdd5644507cd53b2
Jon Beck <jonnydbeck@gmail.com>
2022-11-17 02:38:33 -0500

    Fixed Category POST

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,13 +42,12 @@ router.post('/', async (req, res) => {
       "category_name": "Things"
     }
   */
-    Product.update(req.body)
-    .then(res.status(200).json())
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
-
+      try {
+        const categoryData = await Category.create(req.body);
+        res.status(200).json(categoryData);
+      } catch (err) {
+        res.status(400).json(err);
+      }    
 });
 
 router.put('/:id', async (req, res) => {
